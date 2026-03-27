55ef1cde3c15f6bc3812d26444a52a5c8bef906b
CLEMENT-MUSY <cm25.sja@gmail.com>
2026-03-27 17:14:00 +0100

    debut de la page d'intr

--- src/hub.php
+++ src/hub.php
@@ -8,7 +8,25 @@
     <title>hub</title>
 </head>
 <body>
-<h1>Hub noa bien joué pour le cours eze</h1>
+    <header class="navbar"
+            <div class="contrainer navbar-content"
+                 <div class="nav-action"
+                    <a href="#" class="btn btn-primary">+ Ajouter un objet</a>
+                    <a href="#" class="btn btn-text">Connexion</a>
+                    <div class="dropdown"
+                        <button class="btn btn-secondary dropdown-toggle">Mon Espace ▼</button>
+                        <div class="dropdown-menu">
+                            <a href="#">Mon Porte-monnaie (16 €) pour kenneth</a>
+                            <a href="#">Déconnexion</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
+
+
 
 </body>
 </html>
