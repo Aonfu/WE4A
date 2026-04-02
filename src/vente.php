656470cb250be9d0ceace450ee8ac84e935777bd
Adrien <adrien.djouder@gmail.com>
2026-04-02 16:19:07 +0200

    je pense avoir fini le form de l'ajout de produit, l'ajout de photo etait particluièrement dur

--- src/vente.php
+++ src/vente.php
@@ -20,8 +20,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $categorie = $_POST["categorie"];
     $description = $_POST["description"];
     $photo = $_FILES["photo"];
-    $db_photo = __DIR__."/src/ressources/images/" . $photo["name"];
+    //l'ia ma suggerer de faire ca pour enregister les photos je pense avoir compris a 67% ce que j'ai fait et ca a pris du temps a debug (com a suppr)
+    $db_photo = __DIR__."/ressources/images/" . $photo["name"];
     move_uploaded_file($photo["tmp_name"],$db_photo);
+    //
     $prix = $_POST["prix"];
     $stmt = $conn->prepare("INSERT INTO produit (id_utilisateur, nom, id_categorie, description, photo, prix_depart) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("isissi", $_SESSION["id"],$nom, $categorie, $description, $db_photo, $prix);
