32503233dceb97d791f05ac9f3d12455f9ba51dc
Loic <loicmorin0105@gmail.com>
2026-04-11 00:36:29 +0200

    Catalogue avec un PHP qui fonctionne pas

---
+++ src/catalogue.php
@@ -0,0 +1,92 @@
+<?php
+$conn = new mysqli(
+    $_ENV['MYSQL_HOST'],
+    $_ENV['MYSQL_USER'],
+    $_ENV['MYSQL_PASSWORD'],
+    $_ENV['MYSQL_DATABASE']
+);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+session_start();
+$requete = "SELECT produit.nom, produit.description, produit.photo, enchere.prix FROM produit JOIN enchere ON produit.id_produit = enchere.id_produit;";
+$resultat = $conn->prepare($requete);
+$resultat->execute();
+$data = $resultat->fetchAll(); /* Je crois que ça fonctionne pas pour Docker */
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Catalogue</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="..." crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="ressources/style/style.css">
+</head>
+<body class="background">
+<nav class="navbar navbar-expand-md navbar-light bg-warning-subtle flex-nowrap">
+    <div class="container-fluid d-flex align-items-center">
+        <a class="navbar-brand me-3 pawnstar-font" href="index.php">PawnStar?</a>
+        <div class="collapse navbar-collapse order-2 order-md-1" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-0 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active pawnstar-font" aria-current="page" href="index.php">Hub</a>
+                </li>
+                <?php
+                if (isset($_SESSION["id"])) {
+                    echo
+                    '<li class="nav-item">
+                            <a class="nav-link pawnstar-font" href="mon_espace.php">Mon Espace</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pawnstar-font" href="vente.php">Vente</a>
+                        </li>';
+                } else {
+                    echo
+                    '<li class="nav-item">
+                            <a class="nav-link pawnstar-font" href="connexion.php">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pawnstar-font" href="inscription.php">Inscription</a>
+                        </li>';
+                }?>
+            </ul>
+        </div>
+        <form class="d-flex order-1 order-md-2" role="search">
+            <input class="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn search-button" type="submit"></button>
+        </form>
+        <button class="navbar-toggler order-3 ms-auto navbar-burger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+<div class="centrer">
+<div class="card-grid">
+    <?php
+    foreach ($data as $enchere) {
+        echo
+                '<div class="card">
+            <div class="card-images">
+                <img src="ressources/img/' . $enchere["photo"] . '" class="card-img-top" alt="...">
+                <img src="ressources/img/scotch.png" class="scotch-1" alt="...">
+                <img src="ressources/img/scotch.png" class="scotch-2" alt="...">
+            </div>
+            <div class="card-body">
+                <h3 class="card-text card-title">' . $enchere["nom"] . '</h3>
+                <p class="card-text">' . $enchere["description"] . '</p>
+                <h1 class="card-text pawnstar-font">' . $enchere["prix"] . '$</h1>
+            </div>
+            <a href="index.php" class="stretched-link text-decoration-none"></a>
+        </div>';
+    } /* Je ne sais pas comment sont gérées les photos et leur extensions. De plus il faudrait quelque chose pour que chaque enchère ait sa page web */
+    ?>
+</div>
+</div>
+</body>
+</html>
+
+
+
