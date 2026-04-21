127c58916bbd327e107d25b85125ff478c699fa5
LoicMorin30 <loicmorin0105@gmail.com>
2026-04-21 11:20:29 +0200

    Update mon_espace.php

--- src/mon_espace.php
+++ src/mon_espace.php
@@ -4,7 +4,6 @@ include "header.php";
 include "footer.php";
 
 date_default_timezone_set('Europe/Paris'); //ligne assez importante pour régler un bug
-session_start();
 $conn = new mysqli(
     $_ENV['MYSQL_HOST'],
     $_ENV['MYSQL_USER'],
