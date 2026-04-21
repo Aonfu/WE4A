5093baa67b72b01e567de0d49466d051e2a2e310
LoicMorin30 <loicmorin0105@gmail.com>
2026-04-21 11:20:43 +0200

    Remove session_start() from vente.php
    Removed session_start() to address session management.

--- src/vente.php
+++ src/vente.php
@@ -5,7 +5,6 @@ include "footer.php";
 
 date_default_timezone_set('Europe/Paris'); //ligne assez importante pour régler un bug
 
-session_start();
 $conn = new mysqli(
     $_ENV['MYSQL_HOST'],
     $_ENV['MYSQL_USER'],
