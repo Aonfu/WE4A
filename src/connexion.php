a9caefd55667de623186fc5f2100f2088fe51aa2
Adrien <adrien.djouder@gmail.com>
2026-04-30 09:28:33 +0200

    mini correction

--- src/connexion.php
+++ src/connexion.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                         <label class="card-text left" for="mdp">Mot de Passe:</label>
                         <input class="form-control form-input" type="password" id="mdp" name="mdp" placeholder="Mot de Passe" required>
                     </div>
-                    <button class="button" type="submit">S'inscrire</button>
+                    <button class="button" type="submit">Se Connecter</button>
                 </form>
             </div>
         </div>
